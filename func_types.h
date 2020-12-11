19731ed3f788eb69e1596c791f99ffb0dea737ca
iulian <iulian.rusu999@gmail.com>
2020-12-12 00:55:03 +0200

    Some cleaning up and refactoring.

--- func_types.h
+++ func_types.h
@@ -8,7 +8,7 @@
 #include <type_traits>
 #include <string>
 #include <stdexcept>
-/* Include GCC-specific name de-mangling header. */
+/* Include GCC-specific ptr_type_name de-mangling header. */
 #ifdef GCC_DEFINED
 #include <cxxabi.h>
 #endif
@@ -22,16 +22,18 @@
 template<typename return_t, typename ... param_t>
 class func_t
 {
-    static const std::string type_name;
     return_t (*func_ptr)(param_t ...);
 
 public:
-    constexpr func_t() noexcept : func_ptr(defaults::default_func)
+    static const std::string_view ptr_type_name;
+    static const std::string_view class_type_name;
+
+    constexpr func_t() noexcept: func_ptr(defaults::default_func)
     {}
 
-    constexpr func_t(const func_t<return_t, param_t ...> &other) noexcept  = default;
+    constexpr func_t(const func_t<return_t, param_t ...> &other) noexcept = default;
 
-    constexpr func_t(return_t (*func)(param_t ...)) noexcept : func_ptr(func)
+    constexpr func_t(return_t (*func)(param_t ...)) noexcept: func_ptr(func)
     {}
 
     func_t &operator=(const return_t (*&other)(param_t ...)) noexcept
@@ -42,15 +44,10 @@ public:
 
     func_t &operator=(const func_t<return_t, param_t ...> &other) noexcept
     {
-        if(this == &other)
+        if (this == &other)
             return *this;
         this->func_ptr = other.func_ptr;
         return *this;
-    }
-
-    static std::string_view name() noexcept
-    {
-        return std::string_view(type_name);
     }
 
     /**
@@ -61,7 +58,7 @@ public:
      * @return A value of type return_t, the result of the function call.
      */
     template<typename maybe_void_t = return_t,
-            typename std::enable_if_t<!std::is_void_v<maybe_void_t>>* = nullptr>
+            typename std::enable_if_t<!std::is_void_v<maybe_void_t>> * = nullptr>
     [[nodiscard]] return_t operator()(param_t ... params) const
     {
         return func_ptr(params ...);
@@ -75,12 +72,11 @@ public:
     * @return A value of type return_t, the result of the function call.
     */
     template<typename maybe_void_t = return_t,
-            typename std::enable_if_t<std::is_void_v<maybe_void_t>>* = nullptr>
+            typename std::enable_if_t<std::is_void_v<maybe_void_t>> * = nullptr>
     void operator()(param_t ... params) const
     {
         func_ptr(params ...);
     }
-
 
     /**
      * Returns a lambda that captures a partial state of the original function.
@@ -90,11 +86,29 @@ public:
      * @return A lambda capturing the partial state of the function.
      */
     template<typename ... partial_param_t>
-    [[nodiscard]] auto partial(partial_param_t ... params)  const noexcept
+    [[nodiscard]] auto partial(partial_param_t ... params) const noexcept
     {
-        return [&](auto ... rest)
+        return [&](auto &&... rest)
         {
             return func_ptr(params ..., rest ...);
+        };
+    }
+    /**
+     * Returns a lambda that captures the functional composition of this object and the received object.
+     * This method requires that param_t is of size 1.
+     * f.composed_with(g)(x) <=> (f o g)(x) <=> f(g(x))
+     *
+     * @tparam other_param_t The parameters received by the other function.
+     * @param other_func A constant reference to the composition target.
+     * @return A lambda that represents the composition.
+     */
+    template<typename ... other_param_t>
+    [[nodiscard]] auto composed_with(func_t<param_t ..., other_param_t ...> const& other_func) const noexcept
+    {
+        static_assert(sizeof ...(param_t) == 1, "function with multiple parameters cannot be composed");
+        return [&](other_param_t ... params)
+        {
+            return func_ptr(other_func.func_ptr(params ...));
         };
     }
 
@@ -102,37 +116,40 @@ public:
     {
     public:
         template<typename maybe_void_t = return_t,
-                typename std::enable_if_t<!std::is_void_v<maybe_void_t>>* = nullptr>
+                typename std::enable_if_t<!std::is_void_v<maybe_void_t>> * = nullptr>
         static return_t default_func([[maybe_unused]] param_t ... params)
         {
             static_assert(std::is_default_constructible_v<return_t>,
-                    "return type of default function must be default constructible");
+                          "return type of default function must be default constructible");
             return return_t{};
         }
 
         template<typename maybe_void_t = return_t,
-                typename std::enable_if_t<std::is_void_v<maybe_void_t>>* = nullptr>
+                typename std::enable_if_t<std::is_void_v<maybe_void_t>> * = nullptr>
         static void default_func([[maybe_unused]] param_t ... params) noexcept
         {}
     };
-
-private:
-    static const char* get_type_name()
-    {
-        const char* result_name = typeid(func_t).name();
-#ifdef GCC_DEFINED
-        // De-mangling uses GCC-specific functions.
-        int error_code;
-        char *demangled_name = abi::__cxa_demangle(result_name, NULL, NULL, &error_code);
-        if(!error_code)
-            result_name = demangled_name;
-#endif
-        return result_name;
-    }
 };
 
+template<typename T>
+static const char* demangled_type_name()
+{
+    const char* result_name = typeid(T).name();
+#ifdef GCC_DEFINED
+    // De-mangling uses GCC-specific functions.
+    int error_code;
+    char *demangled_name = abi::__cxa_demangle(result_name, NULL, NULL, &error_code);
+    if(!error_code)
+        result_name = demangled_name;
+#endif
+    return result_name;
+}
+
 template<typename return_t, typename ... param_t>
-const std::string func_t<return_t, param_t ...>::type_name = func_t::get_type_name();
+const std::string_view func_t<return_t, param_t ...>::ptr_type_name = demangled_type_name<return_t (*)(param_t ...)>();
+
+template<typename return_t, typename ... param_t>
+const std::string_view func_t<return_t, param_t ...>::class_type_name = demangled_type_name<func_t>();
 
 template<typename ... param_t>
 using void_func_t = func_t<void, param_t ...>;
--- main.cpp
+++ main.cpp
@@ -23,20 +23,23 @@ int main()
 
     // OK, int has a default constructor, calling f2 will return 0
     func_t<int> f2;
-    std::cout << "Type of f2 is " << f2.name() << '\n';
+    std::cout << "Type of f2 is:\t\t\t\t\t" << f2.class_type_name << '\n';
+    std::cout << "Type of f2's pointer is:\t\t" << f2.ptr_type_name << '\n';
     std::cout << "f2() = " << f2() << '\n' << '\n';
 
     // creating a partial from a given function
     func_t sum_func = sum_them;
-    std::cout << "Type of sum_func is " << sum_func.name() << '\n';
+    std::cout << "Type of sum_func is:\t\t\t\t" << sum_func.class_type_name << '\n';
+    std::cout << "Type of sum_func's pointer is:\t\t" << sum_func.ptr_type_name << '\n';
     // partial_func will capture the first parameter to be 10
     auto partial_func = sum_func.partial(10);
     // call the partial, providing the rest of the parameters
-    std::cout << "sum_func.partial(10)(20, 30) = " <<  partial_func(20u, 30L) << '\n' << '\n';
+    std::cout << "sum_func.(10, 20, 30) = " <<  partial_func(20u, 30L) << '\n' << '\n';
 
     // we can also make functions that take other functions as parameters
     func_t applier = apply<long, int, unsigned, long>;
-    std::cout << "Type of applier is " << applier.name() << '\n';
+    std::cout << "Type of applier is:\t\t\t\t\t" << applier.class_type_name << '\n';
+    std::cout << "Type of applier's pointer is:\t\t" << applier.ptr_type_name << '\n';
     // equivalent to sum_func(1, 2, 3)
     std::cout << "applier(sum_func, 1, 2, 3) = " << applier(sum_func, 1, 2u, 3L) << '\n' << '\n';
 }
