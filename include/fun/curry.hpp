c8797c3fe37161f51553bd7b7e78259a63e668e7
Iulian <iulian.rusu999@gmail.com>
2022-06-23 19:22:23 +0300

    Fix `noexcept` propagation in `uncurry`

--- include/fun/curry.hpp
+++ include/fun/curry.hpp
@@ -12,14 +12,14 @@ namespace fun
         struct is_nothrow_if_invocable : std::true_type {};
 
         template<typename F, typename... Args>
-        requires std::is_invocable_v<F &&, Args &&...>
-        struct is_nothrow_if_invocable<F, Args ...> : std::is_nothrow_invocable<F &&, Args &&...> {};
+        requires std::is_invocable_v<F, Args ...>
+        struct is_nothrow_if_invocable<F, Args ...> : std::is_nothrow_invocable<F, Args ...> {};
 
         template<typename F, typename... Args>
         inline constexpr bool is_nothrow_if_invocable_v = is_nothrow_if_invocable<F, Args ...>::value;
 
         template<typename F, typename... Args>
-        constexpr decltype(auto) curry_impl(F &&f, Args &&... args) noexcept(is_nothrow_if_invocable_v<F, Args ...>)
+        constexpr decltype(auto) curry_impl(F &&f, Args &&... args) noexcept(is_nothrow_if_invocable_v<F &&, Args &&...>)
         {
             if constexpr (requires { std::invoke(std::forward<F>(f), std::forward<Args>(args) ...); })
                 return std::invoke(std::forward<F>(f), std::forward<Args>(args) ...);
@@ -36,7 +36,7 @@ namespace fun
         }
 
         template<typename F, typename Arg, typename... Args>
-        constexpr decltype(auto) uncurry_impl(F &&f, Arg &&arg, Args &&... args) noexcept
+        constexpr decltype(auto) uncurry_impl(F &&f, Arg &&arg, Args &&... args) noexcept(is_nothrow_if_invocable_v<F &&, Arg &&>)
         {
             if constexpr (sizeof... (Args) == 0)
                 return std::invoke(std::forward<F>(f), std::forward<Arg>(arg));
@@ -83,7 +83,8 @@ namespace fun
             return std::forward<F>(f);
         else
             return [captured = std::make_tuple(std::forward<F>(f))]
-                <typename Arg, typename... Args>(Arg &&arg, Args &&... args) mutable noexcept -> decltype(auto) {
+                <typename Arg, typename... Args>(Arg &&arg, Args &&... args)
+                mutable noexcept(std::is_nothrow_invocable_v<F &&, Arg &&>) -> decltype(auto) {
                     return detail::uncurry_impl(
                         std::invoke(std::forward<F>(std::get<0>(captured)), std::forward<Arg>(arg)),
                         std::forward<Args>(args) ...
--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ int main()
     auto original = [](int x, int y, int z) { return x + y + z; };
     auto curried = fun::curry(original);
     auto uncurried = fun::uncurry(curried);
-    std::cout << original(11, 22, 33) << ' ' << curried(11)(22)(33) << ' ' << uncurried(11, 22, 33);
+    std::cout << original(1, 2, 3) << '\n';
+    std::cout << curried(1)(2)(3) << '\n';
+    std::cout << uncurried(1, 2, 3) << '\n';
 }
 
